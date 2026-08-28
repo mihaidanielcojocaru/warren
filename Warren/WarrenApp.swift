78512b58ca1c2162ec935efdd83d21a18400728b
Mihai Cojocaru <mihaicojocaru>
2026-08-28 13:38:17 +0300

    Initial Commit

---
+++ Warren/WarrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarrenApp.swift
+//  Warren
+//
+//  Created by Mihai Cojocaru on 28/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct WarrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
